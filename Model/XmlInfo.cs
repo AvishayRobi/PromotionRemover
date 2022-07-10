81b22418d56f6bab1c7b146d8204cd1cce55e1b1
AvishayRobi <79645481+AvishayRobi@users.noreply.github.com>
2022-07-10 13:10:37 +0300

    Create XmlInfo.cs

---
+++ Model/XmlInfo.cs
@@ -0,0 +1,11 @@
+namespace PromotionRemover.Model
+{
+  public class XmlInfo
+  {
+    public string FetchFullPath { get; set; }
+
+    public string SaveFullPath { get; set; }
+
+    public string Namespace { get; set; }
+  }
+}
